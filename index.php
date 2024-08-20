36c61c096759952f7318845c7615932015c81c2d
Keybet12 <125263490+Keybet12@users.noreply.github.com>
2024-08-20 23:36:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if (isset($_POST['submit'])) {
             
             echo "<script>
                     alert('Login successful!');
-                    window.location.href = 'Index4.php';
+                    window.location.href = 'https://blooming-badlands-84005-a8db784487d5.herokuapp.com/Index4.php';
                   </script>";
             exit();
         } else {
@@ -45,7 +45,7 @@ $conn->close();
 
 $clientID = '222201930065-dd34sbsahndgmj0k5le0rm4m0059o96v.apps.googleusercontent.com';
 $clientSecret = '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';
-$redirectUri =  header("Location: Index4.php");
+$redirectUri =  'https://blooming-badlands-84005-a8db784487d5.herokuapp.com/Index4.php';
 
 // create Client Request to access Google API
 $client = new Google_Client();
