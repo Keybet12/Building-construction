8a0fddb4eef9f560d2c03a0b7008b0676c673364
Keybet12 <125263490+Keybet12@users.noreply.github.com>
2024-08-21 15:08:20 +0300

    Update callback_url.php

--- callback_url.php
+++ callback_url.php
@@ -10,13 +10,34 @@
      $mpesaResponse = file_get_contents('php://input');
  
      // log the response
-     $logFile = "M_PESAConfirmationResponse.txt";
+     $logFile = "M_PESAConfirmationResponse.json";
  
      // write to file
      $log = fopen($logFile, "a");
  
      fwrite($log, $mpesaResponse);
      fclose($log);
- 
-     echo $response;
+
+     $mpesaResponse = file_get_contents('M_PESAConfirmationResponse.json');
+     $callbackContent = json_decode($mpesaResponse);
+
+     $Resultcode = $callbackContent->Body->stkCallback->ResultCode;
+     $CheckoutRequestID = $callbackContent->Body->stkCallback->CheckoutRequestID;
+     $PhoneNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[4]->value;
+     $formatedPhone = str_replace("254", "0", $PhoneNumber);
+     $MpesaReceiptNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[1]->value;
+     $Amount = $callbackContent->Body->stkCallback->CallbackMetadata->Item[0]->value;
+
+    if($Resultcode == 0){
+
+         include('connectionForeman.php');
+
+        $insert = $conn->query("INSERT INTO payment(CheckoutRequestID, ResultCode, amount, MpesaReceiptNumber, PhoneNumber)");
+         $conn = null;
+    }
+
+echo $response;
+
+
+
 ?>
