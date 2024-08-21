3ef41bc338a8b7479f7a3f948a8fa99b0ef3aa26
Keybet12 <125263490+Keybet12@users.noreply.github.com>
2024-08-21 15:19:41 +0300

    Update callback_url.php

--- callback_url.php
+++ callback_url.php
@@ -23,17 +23,22 @@
 
      $Resultcode = $callbackContent->Body->stkCallback->ResultCode;
      $CheckoutRequestID = $callbackContent->Body->stkCallback->CheckoutRequestID;
+     $Amount = $callbackContent->Body->stkCallback->CallbackMetadata->Item[0]->value;
+     $MpesaReceiptNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[1]->value;
      $PhoneNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[4]->value;
      $formatedPhone = str_replace("254", "0", $PhoneNumber);
-     $MpesaReceiptNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[1]->value;
-     $Amount = $callbackContent->Body->stkCallback->CallbackMetadata->Item[0]->value;
 
     if($Resultcode == 0){
 
-         include('connectionForeman.php');
+        $conn = mysqli_connect("sql5.freesqldatabase.com", "sql5726945", "UIEGnP4mm3", "sql5726945");
+ if($conn == false) {
+    die("Connection Error". mysqli_connect_error());
+  }
+ else{
 
         $insert = $conn->query("INSERT INTO payment(CheckoutRequestID, ResultCode, amount, MpesaReceiptNumber, PhoneNumber)");
          $conn = null;
+ }
     }
 
 echo $response;
