165f35b7f557fb5673279dcb1f77729133cc3bc5
Keybet12 <125263490+Keybet12@users.noreply.github.com>
2024-08-21 09:55:27 +0300

    Update Index4.php

--- Index4.php
+++ Index4.php
@@ -1,5 +1,10 @@
 
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (isset($_POST['submit'])) {
     date_default_timezone_set('Africa/Nairobi');
 
