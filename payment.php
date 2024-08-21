8024532e23d46936e07c8916014bcdeefa10a12e
Keybet12 <125263490+Keybet12@users.noreply.github.com>
2024-08-21 15:54:31 +0300

    Update payment.php

--- payment.php
+++ payment.php
@@ -103,7 +103,7 @@
       </div>
     </div>
     <script
-      type="text/javascript"
+     type="text/javascript"
       src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
     ></script>
     <script type="text/javascript" src=""></script>
